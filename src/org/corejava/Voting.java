c8acff6f5090dd1cc99836f3581f8842e4b363cd
seleniumsabari <mail2vipsabari@gmail.com>
2019-09-05 20:09:25 +0530

    Recently Added

---
+++ src/org/corejava/Voting.java
@@ -0,0 +1,17 @@
+package org.corejava;
+
+import java.util.Scanner;
+
+public class Voting {
+      public static void main(String[] args) {
+		Scanner sn=new Scanner(System.in);
+		System.out.println("Enter Your Age :");
+		int n = sn.nextInt();
+		if(n>=18) {
+			System.out.println("Eligible to vote");
+		}
+		else {
+			System.out.println("Not Eligible to vote");
+		}
+	}
+}
